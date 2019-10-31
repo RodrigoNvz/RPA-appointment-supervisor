ecdb9d18a5f00cb5ea67dc15e729aea8aff0947f
RodrigoNvz <rodrigonvz@outlook.es>
2019-10-31 09:34:20 -0600

    return update

--- main.py
+++ main.py
@@ -9,91 +9,63 @@ lateDate="2019-10-18 13:04:00"
 
 
 async def wm_appointment_portal():
-    # print('\n'*60)
-    clear = lambda: os.system('cls')  # on Linux System
-    clear()
-    if(True):
-        start = time.time()
-        browser = await launch(headless=False)
-        strusername = "body > div > div > div > div.page-container > div.main-container > div.content-container > div > div > form > span:nth-child(1) > span > span > input"
-        strpass = "body > div > div > div > div.page-container > div.main-container > div.content-container > div > div > form > span:nth-child(2) > span > span > input"
-        strbtn = "body > div > div > div > div.page-container > div.main-container > div.content-container > div > div > form > button"
 
-        page = await browser.newPage()
-        await page.setViewport({'width': 1024, 'height': 768, 'deviceScaleFactor': 1})
-        page.setDefaultNavigationTimeout(60000)
-        await page.goto('https://retaillink.login.wal-mart.com/?ServerType=IIS1&CTAuthMode=BASIC&language=en&utm_source=retaillink&utm_medium=redirect&utm_campaign=FalconRelease&CT_ORIG_URL=/&ct_orig_uri=/')
-        await page.waitFor(strusername)
+    browser = await launch(headless=False)
+    strusername = "body > div > div > div > div.page-container > div.main-container > div.content-container > div > div > form > span:nth-child(1) > span > span > input"
+    strpass = "body > div > div > div > div.page-container > div.main-container > div.content-container > div > div > form > span:nth-child(2) > span > span > input"
+    strbtn = "body > div > div > div > div.page-container > div.main-container > div.content-container > div > div > form > button"
 
-        username = await page.querySelector(strusername)
-        password = await page.querySelector(strpass)
+    page = await browser.newPage()
+    await page.setViewport({'width': 1024, 'height': 768, 'deviceScaleFactor': 1})
+    page.setDefaultNavigationTimeout(60000)
+    await page.goto('https://retaillink.login.wal-mart.com/?ServerType=IIS1&CTAuthMode=BASIC&language=en&utm_source=retaillink&utm_medium=redirect&utm_campaign=FalconRelease&CT_ORIG_URL=/&ct_orig_uri=/')
+    await page.waitFor(strusername)
 
-        data=readFile(r'walmartD.txt',"txt")
+    username = await page.querySelector(strusername)
+    password = await page.querySelector(strpass)
 
-        await username.type(data[0])
-        await password.type(data[1])
-        await page.click(strbtn)
-        #await page.waitForSelector("Head1")
-        print("Esperando")
-        await page.waitForNavigation()
-        #await page.waitFor(15000)
-        print("YA")
-        contenido = await page.content()
-        print(contenido)
-        print("HIa")
+    data=readFile(r'walmartD.txt',"txt")
 
-#bodyDataDiv > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td > div > input[type=text]:nth-child(3)
-        print("LOGIN SUCCESS")
-        #targetsAntes = len(browser.targets())
-        #print(targetsAntes)
-        #await page.waitFor(5000)
-        #Executing internal func
-        #await browser.browserContext
-        #await page.evaluate('() => openWin(\'/navis/default.aspx\',\'Appointment_Scheduling\',640,480)')
-        
-        #await page.waitFor(5000)
+    print("Filling form...")
+    await username.type(data[0])
+    await password.type(data[1])
+    await page.click(strbtn)
 
-        testpage = await browser.newPage()
-        await testpage.goto("https://retaillink.wal-mart.com/navis/default.aspx")
-        await testpage.waitForNavigation()
-        #await testpage.goto("https://logistics-scheduler-www9.wal-mart.com/retaillink_mx/ServerDecision.jsp")
-        await testpage.waitForNavigation()
-        #await testpage.goto("https://logistics-scheduler-www9.wal-mart.com/retaillink_mx/NavisDecrypt.jsp")
-        await testpage.waitForNavigation()
-        print("LISTOOOOOO")
-        await testpage.goto("https://logistics-scheduler-www9.wal-mart.com/trips_mx/quickQuery.do?type=thisWeeksDeliveries")
-        #await testpage.waitForNavigation()
-        print("LISTOOOOOO 2")
-        contenidotest = await testpage.content()
-        print(contenidotest)
-        #bodyDataDiv > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(5) > td > div > input[type=text]:nth-child(3)
-        xtabla = await testpage.waitForXPath("//*[@id=\"SortTable0\"]/tbody")
-        tabla = await testpage.evaluate('(xtabla) => xtabla.children', xtabla)
-        #print(len(tabla))
+    await page.waitForNavigation()
+    print("Succesful login...Navigating")
 
-        for i in range(1,len(tabla)+1):
-            index = str(i)
-            x1 = await testpage.waitForXPath("//*[@id=\"SortTable0\"]/tbody/tr[{}]/td[1]/a".format(index))
-            no_entrega = await testpage.evaluate('(x1) => x1.innerText', x1)
-            x2 = await testpage.waitForXPath("//*[@id=\"SortTable0\"]/tbody/tr[{}]/td[8]".format(index))
-            cita = await testpage.evaluate('(x2) => x2.innerText', x2)
-            print("Resultado {}: ".format(index), no_entrega, cita)
-        #await testpage.waitFor("body > mc > tbody > tr:nth-child(2) > td.contentPanel > table > tbody > tr:nth-child(4) > td > form > table > tbody > tr > td > table > tbody > tr.contentBodyRow > td.contentBody > SortTable0 > thead > tr.tableTitleRow > th > table > tbody > tr > td")
-        print("LISTOOOOOO 3")
-        print('SUCCESSSS!!!!')
-        await page.waitFor(10000)
-        await page.close()
-        try:
-            await browser.close()
-        except:
-            print('Error al cerrar, same as always')
+    #Opening query session
+    testpage = await browser.newPage()
+    await testpage.goto("https://retaillink.wal-mart.com/navis/default.aspx")
+    await testpage.waitForNavigation()
+    await testpage.waitForNavigation()
+    await testpage.waitForNavigation()
 
-#favoritesList
-        end = time.time()
-        
-        print('PROCESS EXECUTION TIME: ', (end - start) / 60, ' MIN')
-        now = datetime.datetime.now()
-        print('\n\n\n\n\n ***** SUCCESSFULL ', now.strftime("%Y-%m-%d %H:%M"), ' ***** \n\n\n\n\n')
+    #Opening this week Deliveries
+    await testpage.goto("https://logistics-scheduler-www9.wal-mart.com/trips_mx/quickQuery.do?type=thisWeeksDeliveries")
+    xtabla = await testpage.waitForXPath("//*[@id=\"SortTable0\"]/tbody")
+    #Extracting table size
+    tabla = await testpage.evaluate('(xtabla) => xtabla.children', xtabla)
+    master_citas = []
+
+    for i in range(1,len(tabla)+1):
+        index = str(i)
+        x1 = await testpage.waitForXPath("//*[@id=\"SortTable0\"]/tbody/tr[{}]/td[1]/a".format(index))
+        no_entrega = await testpage.evaluate('(x1) => x1.innerText', x1)
+        x2 = await testpage.waitForXPath("//*[@id=\"SortTable0\"]/tbody/tr[{}]/td[8]".format(index))
+        cita = await testpage.evaluate('(x2) => x2.innerText', x2)
+        #Conversión a datetime
+        clean_cita = datetime.datetime.strptime(cita, '%Y-%m-%d %H:%M:%S')
+        master_citas.append([no_entrega,clean_cita])
+
+    print("Succesful extraction... \nResults:\n",master_citas)
+    await page.waitFor(5000)
+    try:
+        await browser.close()
+    except:
+        print('Error al cerrar, same as always')
+    return master_citas
+
 
 
 #Example of route call
